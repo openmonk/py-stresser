afe91dd4f8408deeba839d8c9892bebcc3809b5b
akuznets <akuznetsou@libertyglobal.com>
2019-09-20 13:23:25 +0200

    another commit

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, url_for
 
 application = Flask(__name__)
 
 @application.route("/")
 def index():
-    return "apps index here"
+    links = []
+    for rule in application.url_map.iter_rules():
+        if len(rule.defaults or []) >= len(rule.arguments or []):
+            url = url_for(rule.endpoint, **(rule.defaults or {}))
+            links.append((url, rule.endpoint))
+    return str(links)
 
 @application.route("/hc")
 def healthcheck():
