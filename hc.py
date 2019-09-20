1ec49ef69cb0d3cc5f20f1173f860a7eb8cc818d
akuznets <akuznetsou@libertyglobal.com>
2019-09-20 16:56:39 +0200

    another commit

--- hc.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/hc")
-def hc():
-    return "ok"
--- main.py
+++ main.py
@@ -1,24 +1,52 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request, Response
+from pprint import pformat
+import time
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    links = []
+    rules = []
     for rule in app.url_map.iter_rules():
-        if len(rule.defaults or []) >= len(rule.arguments or []):
-            url = url_for(rule.endpoint, **(rule.defaults or {}))
-            links.append((url, rule.endpoint))
-    from pprint import pformat
-    return pformat(links)
+        methods = ','.join(sorted(rule.methods))
+        rules.append((rule.endpoint, methods, str(rule)))
+    return pformat(rules) + "\n"
 
 @app.route("/hc")
 def healthcheck():
-    return "ok"
+    return "ok\n"
 
 @app.route("/skiller")
 def skiller():
-	return "killed"
+	return "todo:socket killer\n"
+
+@app.route("/fdkill/<nr>")
+def fdkill(nr=1):
+    def mkfd():
+        fds = []
+        for n in range(0, int(nr)):
+            fds.append(open("spamfile." + str(n), "w"))
+            yield "created %s \n" % (pformat(fds[-1]),)
+        yield "sleeping (oncreate)\n"
+        time.sleep(60)
+        for n in range(0, int(nr)):
+            yield "write %s\n" % (pformat(fds[n]),)
+            fds[n].write('stuff')
+        yield "sleeping (onclose)\n"
+        time.sleep(60)
+        for n in range(0, int(nr)):
+            yield "close %s\n" % (pformat(fds[n]),)
+            fds[n].close()
+        yield "sleeping (onremove)\n"
+        time.sleep(60)
+        for n in range(0, int(nr)):
+            fn = "spamfile." + str(n)
+            yield "delete %s\n" % (fn, )
+            os.remove(fn)
+        yield "stress done\n"
+
+    return Response(mkfd(), mimetype="text/plain")
 
 if __name__ == "__main__":
     app.run()
